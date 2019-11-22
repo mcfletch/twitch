a3dc436dcf5533e7bd6fd3c9a97fcb8762eb4b5d
Mike C. Fletcher <mcfletch@vrplumber.com>
2019-11-22 11:53:34 -0500

    Bump version for first python3 release

--- twitch/__init__.py
+++ twitch/__init__.py
@@ -4,4 +4,4 @@ Uses numpy to create raw data-arrays for rendering BSP data-files in
 OpenGL.  Includes both simple and patch faces, with patch faces 
 tessellated into triangles.
 """
-__version__ = '1.0.0a1'
+__version__ = '2.0.0'
