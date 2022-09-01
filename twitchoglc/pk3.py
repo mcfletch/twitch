238e0e16f687428c00905bfad5564b2575ee1e76
Mike C. Fletcher <mcfletch@vrplumber.com>
2022-08-31 22:24:13 -0400

    BSP Choice on loading of a file so that you can load the original q3 pk3 files
    Still missing lots of shaders and textures, however

--- twitchoglc/pk3.py
+++ twitchoglc/pk3.py
@@ -7,71 +7,98 @@ pk3 files generally look like this:
     levelshots/*.jpg
 """
 from __future__ import absolute_import
+from fnmatch import fnmatch
+
 try:
-    unicode 
+    unicode
 except NameError:
     unicode = str
 import os, zipfile, tempfile, hashlib
-MAPS_DIR = os.path.expanduser( '~/.cache/twitch/maps' )
 
-def escape_path( fn ):
-    throwaway_directory = '/tmp/junk/though/'
-    test_fn = os.path.normpath( 
-        os.path.join( throwaway_directory, fn )
-    )
-    return os.path.relpath( test_fn, throwaway_directory ).startswith( '../' )
+MAPS_DIR = os.path.expanduser("~/.cache/twitch/maps")
 
-def scan_for_escape_paths( zipfile ):
-    """Scan zipfile's ZipInfo for paths that would escape the unpack directory
-    """
+
+def escape_path(fn):
+    throwaway_directory = "/tmp/junk/though/"
+    test_fn = os.path.normpath(os.path.join(throwaway_directory, fn))
+    return os.path.relpath(test_fn, throwaway_directory).startswith("../")
+
+
+def scan_for_escape_paths(zipfile):
+    """Scan zipfile's ZipInfo for paths that would escape the unpack directory"""
     for info in zipfile.infolist():
-        if escape_path( info.filename ):
-            raise IOError( "Potentially malicious zip entry found (references file outside directory), aborting")
+        if escape_path(info.filename):
+            raise IOError(
+                "Potentially malicious zip entry found (references file outside directory), aborting"
+            )
         yield info
 
-def key( url ):
+
+def key(url):
     """Trivial mechanism to hash URL into a unique key"""
-    return hashlib.sha1( 
-        url.encode('utf8') if isinstance(url,unicode) else url 
+    return hashlib.sha1(
+        url.encode("utf8") if isinstance(url, unicode) else url
     ).hexdigest()
 
-def unpack_directory( key=None ):
-    """Calculate and create the unpacking directory for the given key
-    """
+
+def unpack_directory(key=None):
+    """Calculate and create the unpacking directory for the given key"""
     if key:
-        path = os.path.join( MAPS_DIR, key )
-        if not os.path.exists( path ):
-            os.makedirs( path )
+        path = os.path.join(MAPS_DIR, key)
+        if not os.path.exists(path):
+            os.makedirs(path)
     else:
-        path = tempfile.mkdtemp( prefix='twitch', suffix='pk3' )
+        path = tempfile.mkdtemp(prefix="twitch", suffix="pk3")
     return path
-        
-def unpack( pk3, directory, no_recurse=False, resources=False ):
+
+
+def bsp_by_pattern(bsps, pattern):
+    for bsp in sorted(bsps):
+        if fnmatch(os.path.basename(bsp), pattern):
+            return bsp
+    return None
+
+
+def unpack(pk3, directory, no_recurse=False, resources=False, bsp_name=None):
     """Unpack a .pk3 file into directory for loading
 
     directory = key( download_url )
     bsp_file = unpack( filename, directory )
-    
+
     returns bsp_file (absolute path location)
     """
     bsps = []
     pk3s = []
-    zip = zipfile.ZipFile( pk3, mode='r' )
-    for info in scan_for_escape_paths( zip ):
-        if os.path.splitext( info.filename )[1].lower() == '.bsp':
-            bsps.append( os.path.join( directory, info.filename) )
-        elif os.path.splitext( info.filename )[1].lower() == '.pk3':
-            pk3s.append( os.path.join( directory, info.filename) )
+    zip = zipfile.ZipFile(pk3, mode="r")
+    for info in scan_for_escape_paths(zip):
+        if os.path.splitext(info.filename)[1].lower() == ".bsp":
+            bsps.append(os.path.join(directory, info.filename))
+        elif os.path.splitext(info.filename)[1].lower() == ".pk3":
+            pk3s.append(os.path.join(directory, info.filename))
     if pk3s and not no_recurse:
         zip.extractall(directory)
         bsp = None
         for pk3 in pk3s:
             return unpack(pk3, directory, no_recurse=True, resources=resources)
     if (not resources) and not bsps:
-        raise IOError( """Did not find any .bsp files in the .pk3 file""" )
-    elif (not resources) and len( bsps ) > 1:
-        raise IOError( """Found %s .bsp files, don't know how to handle that yet"""%( len(bsps)))
-    zip.extractall( directory )
+        raise IOError("""Did not find any .bsp files in the .pk3 file""")
+    elif (not resources) and len(bsps) > 1:
+
+        def available_bsps():
+            return ", ".join(sorted([os.path.basename(x) for x in bsps]))
+
+        if bsp_name:
+            bsp = bsp_by_pattern(bsps, bsp_name)
+            if not bsp:
+                raise IOError(
+                    "Did not find bsp matching %r, found:\n\t%s"
+                    % (bsp_name, available_bsps())
+                )
+            bsps = [bsp]
+        else:
+            raise IOError(
+                """Found %s .bsp files, specify the bsp_name to load from the pk3 file: \n\t%s"""
+                % (len(bsps), available_bsps())
+            )
+    zip.extractall(directory)
     return bsps[0] if bsps else None
-
-
--- twitchoglc/viewer.py
+++ twitchoglc/viewer.py
@@ -1,8 +1,9 @@
 """Renderer for a Twitch node (Quake III style BSP map)"""
 from __future__ import absolute_import
 import OpenGL
-#OpenGL.FULL_LOGGING = True
-import logging,numpy, sys, threading, os
+
+# OpenGL.FULL_LOGGING = True
+import logging, numpy, sys, threading, os
 from OpenGLContext import testingcontext
 from . import maprender
 from OpenGL.GL import *
@@ -10,67 +11,75 @@ from OpenGL.arrays import vbo
 from OpenGLContext.scenegraph import imagetexture, shape, transform
 from OpenGLContext.scenegraph.text import text
 from OpenGLContext import texture
+
 log = logging.getLogger(__name__)
 BaseContext = testingcontext.getInteractive()
 
-class TwitchContext( BaseContext ):
 
+class TwitchContext(BaseContext):
     def __init__(self, *args, **named):
-        self.target = named.pop('target')
-        super(TwitchContext,self).__init__(*args,**named)
+        self.target = named.pop("target")
+        super(TwitchContext, self).__init__(*args, **named)
 
-    def OnInit( self ):
-        self.renderer = maprender.Map( self.target )
-        threading.Thread( target=self.LoadAndRefresh ).start()
+    def OnInit(self):
+        self.renderer = maprender.Map(self.target)
+        threading.Thread(target=self.LoadAndRefresh).start()
         # default near is far too close for 8 units/foot quake model size
-        self.platform.setFrustum( near = 30, far=50000 )
+        self.platform.setFrustum(near=30, far=50000)
         self.movementManager.STEPDISTANCE = 50
 
-    def LoadAndRefresh( self ):
+    def LoadAndRefresh(self):
         self.renderer.load()
-        self.triggerRedraw( False )
-    def Render( self, mode = None):
+        self.triggerRedraw(False)
+
+    def Render(self, mode=None):
         """Render the geometry for the scene."""
-        BaseContext.Render( self, mode )
+        BaseContext.Render(self, mode)
         if not mode.visible:
             return
         if self.renderer.loaded:
-            glRotatef( -90, 1.0,0,0 )
-            #glScalef( .01, .01, .01 )
+            glRotatef(-90, 1.0, 0, 0)
+            # glScalef( .01, .01, .01 )
             self.renderer.Render(mode)
 
+
 def get_options():
-    import argparse 
+    import argparse
+
     parser = argparse.ArgumentParser(
-        description='Renders a Quake III Style .pk3 file using OpenGLContext'
+        description="Renders a Quake III Style .pk3 file using OpenGLContext"
     )
     parser.add_argument(
-        'target',help='A .bsp (or .pk3) file to (unpack and) parse',
+        "-b",
+        "--bsp",
+        default=None,
+    )
+    parser.add_argument(
+        "target",
+        help="A .bsp (or .pk3) file to (unpack and) parse",
     )
     return parser
 
 
 def main():
-    logging.basicConfig( level = logging.INFO )
-    logging.getLogger('OpenGLContext.scenegraph.text').setLevel(logging.WARNING)
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("OpenGLContext.scenegraph.text").setLevel(logging.WARNING)
     options = get_options().parse_args()
     target = options.target
-    if target.startswith('http://') or target.startswith('https://'):
+    if target.startswith("http://") or target.startswith("https://"):
         from . import downloader
+
         target = downloader.pull_pk3(target)
-    elif (
-        os.path.isfile(target) 
-        and (
-            target.lower().endswith('.pk3') 
-            or target.lower().endswith('.zip')
-        )
+    elif os.path.isfile(target) and (
+        target.lower().endswith(".pk3") or target.lower().endswith(".zip")
     ):
         from . import pk3
+
         url = os.path.basename(target)
         key = pk3.key(url)
         directory = pk3.unpack_directory(key)
         log.info("Unpacking PK3 to %s", directory)
-        target = pk3.unpack(target,directory)
+        target = pk3.unpack(target, directory, bsp_name=options.bsp)
     else:
-        target = options.target 
+        target = options.target
     TwitchContext.ContextMainLoop(target=target)
